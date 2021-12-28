69f17f7dd2efaa1790c6de223e1dc850508e862c
Hazwan <hazzwan12@gmail.com>
2021-12-28 23:29:35 +0800

    Add alluxio start docker image script

---
+++ alluxio/alluxio-master.sh
@@ -0,0 +1,10 @@
+docker run --rm \
+    -p 19999:19999 \
+    -p 19998:19998 \
+    --net=data_platform_network \
+    --name=alluxio-master \
+    -e ALLUXIO_JAVA_OPTS=" \
+       -Dalluxio.master.hostname=alluxio-master \
+       -Dalluxio.master.mount.table.root.ufs=/opt/alluxio/underFSStorage \
+       -Dalluxio.security.authorization.permission.enabled=false" \
+    alluxio/alluxio master
---
+++ alluxio/alluxio-worker.sh
@@ -0,0 +1,11 @@
+docker run --rm \
+    -p 29999:29999 \
+    -p 30000:30000 \
+    --net=data_platform_network \
+    --name=alluxio-worker \
+    --shm-size=1G \
+    -e ALLUXIO_JAVA_OPTS=" \
+       -Dalluxio.worker.ramdisk.size=1G \
+       -Dalluxio.master.hostname=alluxio-master \
+       -Dalluxio.worker.hostname=alluxio-worker" \
+    alluxio/alluxio worker
